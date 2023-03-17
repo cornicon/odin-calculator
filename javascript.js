fefac033a30f1870674463290427de945a579f59
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-17 13:16:55 -0700

    Updated With Add Subract Multiply Divide Functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function Add(term1, term2)
+{
+    return term1 + term2;
+}
+
+function Subtract(minuend, subtrahend)
+{
+    return minuend - subtrahend;
+}
+
+function Multiply(multiplier, multiplicand)
+{
+    return multiplier * multiplicand;
+}
+
+function Divide(numerator, denominator)
+{
+    return numerator / denominator;
+}
