de4f48a24e5fd4453cd4d111ed89119b2943c41e
Andrew Silva <4087800+cornicon@users.noreply.github.com>
2023-03-17 13:23:15 -0700

    Added Operate Function

--- javascript.js
+++ javascript.js
@@ -17,3 +17,17 @@ function Divide(numerator, denominator)
 {
     return numerator / denominator;
 }
+
+function Operate(operator, number1, number2)
+{
+    if(operator === 'add')
+        return Add(number1, number2);
+    if(operator === 'subtract')
+        return Subtract(number1, number2);
+    if(operator === 'multiply')
+        return Multiply(number1, number2);
+    if(operator === 'divide')
+        return Divide(number1, number2);
+    else
+        return 'INVALID OPERATION';
+}
